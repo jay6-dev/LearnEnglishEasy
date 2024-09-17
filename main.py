191c76df8a23d6b3d1deef5ecb8af78c1bf0adef
Joyce Nhlengetwa <132927877+jay6-dev@users.noreply.github.com>
2024-09-17 10:08:26 +0200

    Updated Models and added css

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import Flask, request, jsonify
+# Use a pipeline as a high-level helper
 from transformers import pipeline
+
+pipe = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3")
+
+# Load model directly
+from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
+
+processor = AutoProcessor.from_pretrained("openai/whisper-large-v3")
+model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-large-v3")
 import os
 
 # Set your API key directly in the code (replace with your actual key)
